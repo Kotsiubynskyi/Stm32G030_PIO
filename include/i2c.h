a76938110d534064d08526dc8aebc092e39cfc56
Kotsiubynskyi <turboopera@gmail.com>
2024-10-02 13:50:42 +0300

    Update

--- include/i2c.h
+++ include/i2c.h
@@ -3,7 +3,8 @@
 
 #include "stm32g030xx.h"
 
-void initI2C2(void);
-void sendData(I2C_TypeDef *I2C, uint32_t i2cAddress, uint8_t *data, uint8_t size);
+void I2C2_Init(void);
+void sendData(I2C_TypeDef *I2C, uint32_t i2cAddress, uint8_t *data,
+              uint8_t size);
 
 #endif // _I2C_
